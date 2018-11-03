de2000a22985f76d675526d4bbe3a5b52af45eef
Przemysław Nowak <przemyslaw.nowak@fingoweb.com>
2018-11-03 13:01:08 +0100

    feat: init new chart component & install d3 plugin

---
+++ src/components/Chart/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  ChartWrapper,
+} from './styles';
+
+class Chart extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <ChartWrapper>
+        chart component
+      </ChartWrapper>
+    )
+  }
+}
+
+export default Chart;
---
+++ src/containers/Dashboard/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DataTextWrapper = styled.div`
+  background-color: rgba(0,0,0,.1);
+  margin-top: 10px;
+  padding: 10px;
+`;
+
+export const DataChartWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+export const HeaderLine = styled.h5`
+  margin: 0 0 10px;
+`;
